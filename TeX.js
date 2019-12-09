c5c73d9ba9996d72739929f0721dae333e3cb2dd
SybelBlue <lbc.dna@gmail.com>
2019-12-09 10:49:18 -0600

    Removed TeXjs

--- TeX.js
+++
@@ -1,3 +0,0 @@
-// start a node instance to run the transpiler
-const Transpiler = require('./transpiler');
-new Transpiler().compileFile('./local/tex/advanced/main.tex', './local/tex/advanced/transpiled.tex');
